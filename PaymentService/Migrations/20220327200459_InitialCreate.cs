943bb4495d5c2bd3424b679c307281ff06fefb9e
Gotcha7770 <gbakanov@inbox.ru>
2022-03-27 23:05:29 +0300

    PaymentService Initial migration

---
+++ PaymentService/Migrations/20220327200459_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CanPay = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CanPay" },
+                values: new object[,]
+                {
+                    { new Guid("19dbd9a0-542b-4bde-bb01-8a847ef017e1"), false },
+                    { new Guid("ce9c06f4-dfd7-442c-ae42-7753e0a52f80"), true }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
